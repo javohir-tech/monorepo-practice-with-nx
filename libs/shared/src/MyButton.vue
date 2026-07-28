1b0079ad0dcb3671975dffce0f2ea24730747b1c
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-07-28 19:21:05 +0500

    feat(shared): create shared library and configure for cross-app usage

---
+++ libs/shared/src/MyButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <button class="my_button" @click="increment">My button{{ count }}</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref < number > (0)
+
+const increment = () => {
+    count.value += 1
+}
+</script>
+
+<style scoped>
+.my_button {
+    background-color: red;
+    border-radius: 10px;
+    border: 0;
+    padding: 10px;
+}
+</style>
